de22bbc146e1e17238d84ca6f125deb9e0d933de
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-07-16 23:17:52 -0400

    [Dynamic Arrays: added support for negative indexing in an efficient way in our Dynamic array]

--- Array-Based-Sequences/dynamicArrays_impleentation.py
+++ Array-Based-Sequences/dynamicArrays_impleentation.py
@@ -16,10 +16,17 @@ class DynamicArray:
 
     def __getitem__(self, k):
         """Return element at index k."""
+        if k < 0:
+
+            target_index = k * (-1)
+            target_index = target_index % self._n
+            if target_index > 0:
+                target_index = self._n - target_index
+            return self._A[target_index]
 
         if not 0 <= k < self._n:
             raise IndexError('invalid index')
-        return self._A[k]                               # retrieve from array
+        return self._A[k]                              # retrieve from array
 
     def append(self, obj):
         """Add object to end of the array."""
@@ -28,7 +35,7 @@ class DynamicArray:
         self._A[self._n] = obj
         self._n += 1
 
-    def resize(self, c):                                # nonpublic utility
+    def _resize(self, c):                                # nonpublic utility
         """Resize internal array to capacity c."""
         B = self._make_array(c)                         # new (bigger) array
         for k in range(self._n):                        # for each existing value
@@ -39,3 +46,42 @@ class DynamicArray:
     def _make_array(self, c):                           # nonpublic utility
         """Return new array with capacity c."""
         return (c * ctypes.py_object)()                 # see ctypes documentation
+
+
+test_data =[
+    (3, 2, [1, 2, 3, 4]),
+    ('Exception', 3 , [1, 2, 3]),
+    (4, -1, [1, 2, 3, 4]),
+    (3, -2, [1, 2, 3, 4]),
+    (1, -4, [1, 2, 3, 4]),
+    (5, -1, [1, 2, 3, 4, 5]),   # ei = 4, gi = 1
+    (1, -20, [1, 2, 3, 4, 5]),  # ei = 0, gi = 0
+    (3, -18, [1, 2, 3, 4, 5]),  # ei = 2, gi = 3
+    (1, -5**5000000, [1, 2, 3, 4, 5])
+]
+
+for item in test_data:
+    expected = item[0]
+    input_index = item[1]
+    input_arr = item[2]
+
+    dy_arr = DynamicArray()
+    for num in input_arr:
+        dy_arr.append(num)
+
+    # my way of handling exceptions
+    if expected == "Exception":
+        exception_caught = False
+        try:
+            actual = dy_arr[input_index]
+        except:
+            exception_caught = True
+
+        if exception_caught:
+            print("PASS: Exception Raised")
+        else:
+            print("FAILED: Exception NOT Raised")
+
+    else:
+        actual = dy_arr[input_index]
+        print(actual == expected, actual, expected)
