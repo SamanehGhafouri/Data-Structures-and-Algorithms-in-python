9548bab1b611c0288bc9c617cfb38ff1b08c88dd
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-09-01 14:10:54 -0400

    find intersection with O(n) in two ways

---
+++ Experiments/find_intersection_element.py
@@ -0,0 +1,66 @@
+def find_intersection_num(list_a, list_b):
+    new_set = set()
+    intersect_nums = set()
+
+    for i in list_a:
+        new_set.add(i)
+
+    for j in list_b:
+        if j in new_set:
+            intersect_nums.add(j)
+    return intersect_nums
+
+
+li_a = [5, 1, 3, 2, 3, 4]
+li_b = [3, -2, 0, 8, 3, 2, 4, 3]
+print(find_intersection_num(li_a, li_b))
+
+
+
+# ############## Test ################### #
+
+test_data = [
+    ([5, 1, 3, 2, 3, 4], [3, -2, 0, 8, 3, 2, 4, 3], {2, 3, 4}),
+    ([-3, 5, -1, 3, 2, 3, -4], [7, 22, 0, 8, 13, 9, 4], set()),
+    ([], [5, 2, 3, -3, -4,  -1, 3], set()),
+    ([22, 8, 0, 7, 4, 9, 15], [], set()),
+    ([22, 8, 0, 7, 4, 9, 15], [5, 2, 3, 0, -4,  -1, 3], {0}),
+    ([22, 8, 0, 7, 4, 9, 15], [5, 2, 3, 11, -4,  -1, 22], {22}),
+    ([22, 8, -1, 7, 4, 9, 15], [22, 5, 2, 3, 11, -4,  -1], {-1, 22}),
+    ([21, 8, -1, 7, 4, 9, 15], [22, 5, 2, 3, 11, -4,  -1, 15], {-1, 15}),
+    ([5, 2, 8], [22, 5, 2, 3, 11, -4,  8, 15], {2, 5, 8}),
+    ([22, 5, 2, 3, 11, -4,  8, 15], [5, 2, 8], {2, 5, 8})
+]
+
+print("--- TESTS ---")
+for item in test_data:
+    expected = item[2]
+    actual = find_intersection_num(item[0], item[1])
+    print(expected == actual, expected, actual)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def find_intersection_num(list_a, list_b):
+#     count_a = set()
+#     intersect_element = set()
+#
+#     for i in list_a:
+#         count_a.add(i)
+#
+#     for j in list_b:
+#         if j in count_a:
+#             intersect_element.add(j)
+#
+#     return intersect_element
---
+++ Experiments/find_intersection_lessEfficient.py
@@ -0,0 +1,41 @@
+def common_element(list_a, list_b):
+    common = set()
+    while len(list_a) != 0:
+        common.add(list_a.pop())
+
+    i = 0
+    while i < len(list_b):
+        if list_b[i] in common:
+            i += 1
+        else:
+            list_b.pop(i)       # time complexity depends on python how to pop
+    common = set(list_b)        # in worst case O(n^2) in best case O(n)
+    return common
+
+
+li_a = [2, 1, 2, 3]
+li_b = [2, 3, 5, 2]
+print(common_element(li_a, li_b))
+
+
+
+# ############## Test ################### #
+
+test_data = [
+    ([5, 1, 3, 2, 3, 4], [3, -2, 0, 8, 3, 2, 4, 3], {2, 3, 4}),
+    ([-3, 5, -1, 3, 2, 3, -4], [7, 22, 0, 8, 13, 9, 4], set()),
+    ([], [5, 2, 3, -3, -4,  -1, 3], set()),
+    ([22, 8, 0, 7, 4, 9, 15], [], set()),
+    ([22, 8, 0, 7, 4, 9, 15], [5, 2, 3, 0, -4,  -1, 3], {0}),
+    ([22, 8, 0, 7, 4, 9, 15], [5, 2, 3, 11, -4,  -1, 22], {22}),
+    ([22, 8, -1, 7, 4, 9, 15], [22, 5, 2, 3, 11, -4,  -1], {-1, 22}),
+    ([21, 8, -1, 7, 4, 9, 15], [22, 5, 2, 3, 11, -4,  -1, 15], {-1, 15}),
+    ([5, 2, 8], [22, 5, 2, 3, 11, -4,  8, 15], {2, 5, 8}),
+    ([22, 5, 2, 3, 11, -4,  8, 15], [5, 2, 8], {2, 5, 8})
+]
+
+print("--- TESTS ---")
+for item in test_data:
+    expected = item[2]
+    actual = common_element(item[0], item[1])
+    print(expected == actual, expected, actual)
